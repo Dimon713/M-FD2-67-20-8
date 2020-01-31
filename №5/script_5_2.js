267a1478849cd17f9af8d3a8b8fae85ddd7c9f76
Dimon713 <Dimon713@tut.by>
2020-01-31 20:45:19 +0300

    31.01.2020

--- №5/script_5_2.js
+++ №5/script_5_2.js
@@ -1,24 +1,19 @@
 function qeepEqual(a, b) {
-    let an = 0;
-    let bn = 0;
-    for (let akey in a) {
-        an++;
-    }
-    for (let bkey in b) {
-        bn++;
-    }
-    if (an !== bn) {
+    if (Object.keys(a).length !== Object.keys(b).length) {
         return false
     }
-
 
     for (let key in a) {
         key = key;
         if (key in b && a[key] === b[key]) {
             continue;
-        } else if (key in b && typeof a[key] === 'object' && typeof b[key] === 'object' && qeepEqual(a[key], b[key]))
+        } else if (key in b && typeof a[key] === 'object' && typeof b[key] === 'object' && qeepEqual(a[key], b[key])) {
             continue;
-        else {
+        } else if (key in b && typeof a[key] === 'function' && typeof b[key] === 'function' && a[key].toString() === b[key].toString()) {
+            continue
+        } else if (key in b && typeof a[key] === 'null' && typeof b[key] === 'null') {
+            continue
+        } else {
             return false
         }
     }
@@ -29,14 +24,18 @@ let x = {
     value: {
         a: 13
     },
-    b: 'abc'
+    b: 'abc',
+    c: (n) => n * 2,
+    d: null,
 };
 
 let y = {
     value: {
         a: 13
     },
-    b: 'abc'
+    b: 'abc',
+    c: (n) => n * 2,
+    d: null,
 };
 
 console.log(qeepEqual(x, y));
