aaed12fff74e1a0b54b35e1e22c97323a3a53f02
Dimon713 <Dimon713@tut.by>
2020-02-03 17:13:02 +0300

    03.02.2020

--- №6/script_6_1.js
+++ №6/script_6_1.js
@@ -1,19 +1,45 @@
 let calculator = (function(x) {
     let result = 0;
     return {
-        add: (x) => (result += x),
-        plus: (x) => (result += x),
-        minus: (x) => (result -= x),
-        multiplay: (x) => (result *= x),
-        divide: (x) => (result /= x),
-        pow: (a) => (result = result ** a),
-        sqrt: () => (result = Math.sqrt(result)),
-        remainder: (x) => (result %= x),
-        plusMinus: () => (result *= (-1)),
-        percent: (x) => (result = (result * x / 100)),
-        print: () => console.log(result),
-        clear: () => (result = 0),
-
+        add: (x) => {
+            result += x
+        },
+        plus: (x) => {
+            result += x
+        },
+        minus: (x) => {
+            result -= x
+        },
+        multiplay: (x) => {
+            result *= x
+        },
+        divide: (x) => {
+            result /= x
+        },
+        pow: (a) => {
+            result **= a
+        },
+        sqrt: () => {
+            result = Math.sqrt(result)
+        },
+        cbrt: () => {
+            result = Math.cbrt(result)
+        },
+        remainder: (x) => {
+            result %= x
+        },
+        plusMinus: () => {
+            result *= (-1)
+        },
+        percent: (x) => {
+            result = (result * x / 100)
+        },
+        print: () => {
+            console.log(result)
+        },
+        clear: () => {
+            result = 0
+        },
     }
 })();
 
@@ -27,4 +53,3 @@ calculator.print();
 calculator.plusMinus();
 calculator.print();
 calculator.plusMinus();
-calculator.print();
